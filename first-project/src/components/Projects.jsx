78976fb8dad26948b44aa0be84980fce68688d6f
shadoud96 <karlos_m96@hotmail.com>
2024-02-18 19:31:48 -0300

    Correction component Project close button

--- first-project/src/components/Projects.jsx
+++ first-project/src/components/Projects.jsx
@@ -8,6 +8,7 @@ export default function Projects(props){
     const projectInfo = activactionState ? projectsInfo[contextState['projectName']] : null;
 
     function closeProject(){
+        console.log("activado");
         // Restart context variables
         setContextState({
             activationState: false,
@@ -19,11 +20,12 @@ export default function Projects(props){
         return(
             <section id="section-projects" className="">
                 <div id="button-container-projects">
-                    <button id="button-projects" onClick={ (e) =>{ closeProject(); } }i><p id='button-p'></p></button>
+                    <button id="button-projects" onClick={ (e) =>{ closeProject(); } }><p id='button-p'></p></button>
                 </div>
                 <video id="project-video"></video>
                 <div id="description-container">
                     <h1 id="project-name">{projectInfo['name']}</h1>
+                    <h4 id="project-url"><a id="link-project" href={projectInfo['url']} target={"_blank"} rel="noreferrer"><i className="fa-solid fa-link" style={{color: "#ffffff"}}></i></a></h4>
                     <p id="project-description">{projectInfo['about']}</p>
                 </div>
                 <div id="project-technologies">
